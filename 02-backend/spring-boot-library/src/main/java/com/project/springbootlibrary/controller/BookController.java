50f292130e38862c2e4e79ea0e925bb12acd96b5
Sai Kishore <saikishore2397@gmail.com>
2023-03-21 16:53:20 -0400

    Added Security in the spring boot backend and logic to fetch user details from JWT token

--- 02-backend/spring-boot-library/src/main/java/com/project/springbootlibrary/controller/BookController.java
+++ 02-backend/spring-boot-library/src/main/java/com/project/springbootlibrary/controller/BookController.java
@@ -2,6 +2,7 @@ package com.project.springbootlibrary.controller;
 
 import com.project.springbootlibrary.entity.Book;
 import com.project.springbootlibrary.service.BookService;
+import com.project.springbootlibrary.utils.ExtractJWT;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,20 +19,22 @@ public class BookController {
     }
 
     @GetMapping("/secure/currentloans/count")
-    public int currentLoansCount() {
-        String userEmail = "testuser@email.com";
+    public int currentLoansCount(@RequestHeader(value = "Authorization") String token) {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
         return bookService.currentLoansCount(userEmail);
     }
 
     @GetMapping("/secure/ischeckedout/byuser")
-    public boolean checoutBookByUser(@RequestParam Long bookId) {
-        String userEmail = "testuser@email.com";
+    public boolean checoutBookByUser(@RequestHeader(value = "Authorization") String token,
+                                     @RequestParam Long bookId) {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
         return bookService.checkoutBookByUser(userEmail, bookId);
     }
 
     @PutMapping("/secure/checkout")
-    public Book checkoutBook(@RequestParam Long bookId) throws Exception {
-        String userEmail = "testuser@email.com";
+    public Book checkoutBook(@RequestHeader(value = "Authorization") String token,
+                             @RequestParam Long bookId) throws Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
         return bookService.checkoutBook(userEmail, bookId);
     }
 }
