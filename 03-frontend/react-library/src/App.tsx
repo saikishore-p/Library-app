3153e15b5f8ac887df65ae9054f8cd5292751790
Sai Kishore <saikishore2397@gmail.com>
2023-03-21 13:08:42 -0400

    Enabled Authentication for the project using okta's openID connect and added login widget through okta dependencies. Added Routes for sign in and logout and gave functionality for respective buttons

--- 03-frontend/react-library/src/App.tsx
+++ 03-frontend/react-library/src/App.tsx
@@ -1,15 +1,34 @@
 import React from 'react';
-import { Redirect, Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch, useHistory } from 'react-router-dom';
 import './App.css';
 import { BookCheckoutPage } from './layouts/BookCheckoutPage/BookCheckoutPage';
 import { HomePage } from './layouts/HomePage/HomePage';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
+import { oktaConfig } from './lib/oktaConfig';
+import { OktaAuth, toRelativeUrl } from '@okta/okta-auth-js';
+import { Security, LoginCallback } from '@okta/okta-react';
+import LoginWidget from './Auth/LoginWidget';
+
+const oktaAuth = new OktaAuth(oktaConfig);
 
 export const App = () => {
+
+  const customAuthHandler = () => {
+    history.push('/login')
+  }
+
+  const history = useHistory();
+
+  const restoreOriginalUri = async (_oktaAuth: any, originalUri: any) => {
+    history.replace(toRelativeUrl(originalUri || '/', window.location.origin));
+  }
+
+
   return (
     <div className='d-flex flex-column min-vh-100 '>
+      <Security oktaAuth={oktaAuth} restoreOriginalUri={restoreOriginalUri} onAuthRequired={customAuthHandler}>
       <Navbar />
       <div className='flex-grow-1'> {/** This div is to keep footer at the bottom of the page even if there is no content in the webpage */}
         <Switch> {/** Switch allows us to render only one route i.e. only homepage will get rendered when /search is given because of / in the front and it refers to homepage. This can be overcome with exact keyword */}
@@ -25,9 +44,15 @@ export const App = () => {
           <Route path='/checkout/:bookId'>
             <BookCheckoutPage />
           </Route>
-        </Switch>
+          <Route path='/login' render={
+            () => <LoginWidget config={oktaConfig} />
+            } 
+          />
+          <Route path='/login/callback' component={LoginCallback} />
+         </Switch>
       </div>
       <Footer />
+      </Security>
     </div>
   );
 }
---
+++ 03-frontend/react-library/src/lib/oktaConfig.ts
@@ -0,0 +1,8 @@
+export const oktaConfig = {
+    clientId: '0oa8svcmjvYf4lemX5d7',
+    issuer: 'https://dev-68551943.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:3000/login/callback',
+    scopes: ['openid', 'profile', 'email'],
+    pkce: true,
+    disableHttpsCheck: true
+}
